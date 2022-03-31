5f5afa96d015d54b4c7870bbd41d156db4acee39
oalberto96 <ochoapublic@gmail.com>
2022-03-31 15:48:31 -0700

    [add] dag to get csv

---
+++ week-02/airflow/dags/data_ingestion_local.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow")
+
+local_worflow = DAG(
+    "TaxiNYDataDAG",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2021, 1, 1)
+)
+
+URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data'
+URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\')}}.csv'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+
+with local_worflow:
+    wget_task = BashOperator(
+        task_id="wget",
+        # bash_command=f"curl -sSL {url} > {AIRFLOW_HOME}/output.csv"
+        bash_command=f'curl -sSL {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}'
+    )
+
+    ingest_task = BashOperator(
+        task_id="ingest",
+        bash_command=f'ls {AIRFLOW_HOME}'
+    )
+
+    wget_task >> ingest_task
